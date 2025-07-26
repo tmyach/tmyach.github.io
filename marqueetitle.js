7a41a98bdf6b73b025b91e83aa1439918dedc7f3
cettiatess-design <tmzy.fo@protonmail.com>
2025-07-26 01:12:25 -0400

    modified:   Yachimovicz_Tesia_Resume.html new file:   favicon.ico modified:   index.html new file:   marqueetitle.js new file:   notice.css new file:   notice.js modified:   stylesheet.css

---
+++ marqueetitle.js
@@ -0,0 +1,37 @@
+<script language=javascript>
+    var rev = "fwd";
+    function titlebar(val){
+        var msg  = "🕸 ᴡᴇʙꜱᴘɪɴɴᴇʀ | 𝚃.𝙼. 𝚈𝚊𝚌𝚑𝚒𝚖𝚘𝚟𝚒𝚌𝚣 ⋆✴︎˚｡⋆";
+        var res = " ";
+        var speed = 100;
+        var pos = val;
+        msg = "   |-"+msg+"-|";
+        var le = msg.length;
+        if(rev == "fwd"){
+            if(pos < le){
+                pos = pos+1;
+                scroll = msg.substr(0,pos);
+                document.title = scroll;
+                timer = window.setTimeout("titlebar("+pos+")",speed);
+            }
+            else {
+                rev = "bwd";
+                timer = window.setTimeout("titlebar("+pos+")",speed);
+            }
+        }
+        else {
+            if(pos > 0) {
+                pos = pos-1;
+                var ale = le-pos;
+                scrol = msg.substr(ale,le);
+                document.title = scrol;
+                timer = window.setTimeout("titlebar("+pos+")",speed);
+            }
+            else {
+                rev = "fwd";
+                timer = window.setTimeout("titlebar("+pos+")",speed);
+            }
+        }
+    }
+    titlebar(0);
+</script>
