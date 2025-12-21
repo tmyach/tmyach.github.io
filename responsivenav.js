58d10ab649e03e17321424b558a18e453ff6e3d7
tmyach <168230249+tmyach@users.noreply.github.com>
2025-12-21 15:27:02 -0500

    nav button on mobile 2

--- responsivenav.js
+++ responsivenav.js
@@ -1,6 +1,10 @@
-const navToggle = document.querySelector('.nav-toggle');
-const nav = document.querySelector('.nav');
+document.addEventListener('DOMContentLoaded', () => {
+  const navToggle = document.querySelector('.nav-toggle');
+  const nav = document.querySelector('.nav');
 
-navToggle.addEventListener('click', () => {
-  nav.classList.toggle('open');
+  if (navToggle && nav) {
+    navToggle.addEventListener('click', () => {
+      nav.classList.toggle('open');
+    });
+  }
 });
